593d9cd8be9171b928cf61cebebb17acecf2df97
FIlip <filip.szwedo@gmail.com>
2022-01-09 18:46:37 +0100

    additionalInfo added in log model

--- src/models/logModel.js
+++ src/models/logModel.js
@@ -5,13 +5,15 @@ const logSchema = new mongoose.Schema({
     timestamp: { type: String, default: new Date().toUTCString() },
     type: { type: String, required: true },
     message: { type: String, required: true },
+    additionalInfo: { type: String },
     origin: { type: String,  required: true }
 }) 
 
 logSchema.index({
     type: 'text',
     message: 'text',
-    origin: 'text'
+    origin: 'text',
+    additionalInfo: 'text'
 })
 
 export default mongoose.model('Log', logSchema);

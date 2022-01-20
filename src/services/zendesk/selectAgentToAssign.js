d164d0235772064a04534cac17e4b2de6951138a
FIlip <filip.szwedo@gmail.com>
2022-01-20 17:41:03 +0100

    separate zendesk folder for services

--- src/services/zendesk/selectAgentToAssign.js
+++ src/services/zendesk/selectAgentToAssign.js
@@ -6,6 +6,7 @@ const selectAgentToAssign = (agents, lastAssignedAgentId, shiftSchedule) => {
     const currentDate = new Date();
     const currentHour = currentDate.getHours() + 1;
     const currentMinute = currentDate.getMinutes();
+    console.log(currentDate)
 
     let currentlyAvailableAgents = [];
     
@@ -16,6 +17,7 @@ const selectAgentToAssign = (agents, lastAssignedAgentId, shiftSchedule) => {
         if (shiftSchedule.agents.find(e => e === agents[agent].name)) {
             const agentPositionInArray = shiftSchedule.agents.indexOf(agents[agent].name);
             const agentShiftLimits = shiftSchedule.hours[agentPositionInArray].split('-');
+            
             //check if the agent works at the moment with 30 minute offset (so agent working till 5pm will get the tickets assigned till 4:30pm) - if yes then push him to the array
             if (agentShiftLimits[0] <= currentHour && 
                     (agentShiftLimits[1] > currentHour + 1 || 
@@ -31,7 +33,6 @@ const selectAgentToAssign = (agents, lastAssignedAgentId, shiftSchedule) => {
            return [currentlyAvailableAgents[agent].id, currentlyAvailableAgents[agent].name]
         } 
     }
-
 
     if (currentlyAvailableAgents.length > 0)
     return [currentlyAvailableAgents[0].id, currentlyAvailableAgents[0].name]

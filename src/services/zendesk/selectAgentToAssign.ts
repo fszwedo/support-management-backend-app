7e0107526b6d10697c8b8f85b907c569b488a71c
FIlip <filip.szwedo@gmail.com>
2022-07-28 07:32:17 +0200

    remove obsolete console logs

--- src/services/zendesk/selectAgentToAssign.ts
+++ src/services/zendesk/selectAgentToAssign.ts
@@ -11,14 +11,12 @@ const selectAgentToAssign = async (agents: Agent[], getLastAssignedAgentId: Func
 
     //get hour and minute in UTC
     const currentDate = new Date();
-    const currentHour = currentDate.getUTCHours();    
+    const currentHour = currentDate.getUTCHours();
     const currentMinute = currentDate.getUTCMinutes();
-
-    console.log(currentHour, currentMinute)
 
     let currentlyAvailableAgents = [];
     //first select the agents available at the current hour
-    for (let agent in agents) {       
+    for (let agent in agents) {
 
         //if in the array of available agents we have a name of the agent that was returned Zendesk - get his working hours
         if (shiftSchedule.agents.find(e => e === agents[agent].name)) {
@@ -32,26 +30,26 @@ const selectAgentToAssign = async (agents: Agent[], getLastAssignedAgentId: Func
 
             //check if the agent works at the moment with 30 minute offset (so agent working till 5pm will get the tickets assigned till 4:30pm) - if yes then push him to the array
             //agents working till 10pm are getting the tickets assigned till the end of the shift
-            if (agentStartTime <= currentHour && 
-                    (agentEndTime - 1 > currentHour || 
-                    (agentEndTime - 1 === currentHour && currentMinute < 30) ||                     
-                    (agentEndTime === 22 && currentHour === 21))) 
+            if (agentStartTime <= currentHour &&
+                (agentEndTime - 1 > currentHour ||
+                    (agentEndTime - 1 === currentHour && currentMinute < 30) ||
+                    (agentEndTime === 22 && currentHour === 21)))
                     /*then*/ currentlyAvailableAgents.push(agents[agent]);
-        }        
+        }
     }
 
     //then select the agent that should get the ticket assigned
-    
+
     const lastAssignedAgentId = await getLastAssignedAgentId();
 
     for (let agent in currentlyAvailableAgents) {
-        if (currentlyAvailableAgents[agent].id > lastAssignedAgentId.agentId){
-           return [currentlyAvailableAgents[agent].id, currentlyAvailableAgents[agent].name]
-        } 
+        if (currentlyAvailableAgents[agent].id > lastAssignedAgentId.agentId) {
+            return [currentlyAvailableAgents[agent].id, currentlyAvailableAgents[agent].name]
+        }
     }
 
     if (currentlyAvailableAgents.length > 0)
-    return [currentlyAvailableAgents[0].id, currentlyAvailableAgents[0].name]
+        return [currentlyAvailableAgents[0].id, currentlyAvailableAgents[0].name]
 
     return [undefined, undefined]
 }

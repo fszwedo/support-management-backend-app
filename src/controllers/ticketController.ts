734796392100e5694e9313217e925c2b87caa85a
FIlip <filip.szwedo@gmail.com>
2023-02-08 12:38:42 +0100

    switch replacing if/else block

--- src/controllers/ticketController.ts
+++ src/controllers/ticketController.ts
@@ -15,13 +15,28 @@ export default class TicketController {
         let ticket;
         //use appropriate service method for the selected path
         try {
-            if (ticketCategory === 'Account access') ticket = await this.service.createAccountAccessRequest(req.body);
-            else if (ticketCategory === 'Admintool access') ticket = await this.service.createAdminAccessRequest(req.body);
-            else if (ticketCategory === 'Account creation') ticket = await this.service.createAccountCreationRequest(req.body);
-            else if (ticketCategory === 'Platform issue') ticket = await this.service.createProblemReport(req.body);
-            else if (ticketCategory === 'FTP') ticket = await this.service.createFTPRequest(req.body);
-            else if (ticketCategory === 'Reporting') ticket = await this.service.createReportingRequest(req.body);
-            else ticket = await this.service.createGeneralTicket(req.body);
+            switch (ticketCategory) {
+                case 'Account access':
+                    ticket = await this.service.createAccountAccessRequest(req.body);
+                    break;
+                case 'Admintool access':
+                    ticket = await this.service.createAdminAccessRequest(req.body);
+                    break;
+                case 'Account creation':
+                    ticket = await this.service.createAccountCreationRequest(req.body);
+                    break;
+                case 'Platform issue':
+                    ticket = await this.service.createProblemReport(req.body);
+                    break;
+                case 'FTP':
+                    ticket = await this.service.createFTPRequest(req.body);
+                    break;
+                case 'Reporting':
+                    ticket = await this.service.createReportingRequest(req.body);
+                    break;
+                default:
+                    ticket = await this.service.createGeneralTicket(req.body);
+            }
         }
         catch (error) {
             console.log(error)

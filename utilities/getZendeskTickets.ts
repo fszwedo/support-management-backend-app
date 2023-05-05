b2afffb65659acb5e14fbeeede7cd0e824650559
FIlip <filip.szwedo@gmail.com>
2023-05-05 12:25:19 +0200

    proposed workflow changes

--- utilities/getZendeskTickets.ts
+++ utilities/getZendeskTickets.ts
@@ -2,8 +2,9 @@ import 'dotenv/config';
 import makeZendeskRequest from '../src/services/zendesk/authenticationService'
 import reassignTickets from '../src/controllers/ticketReassignmentController'
 
+
 const getZendeskTickets = async () => {
-    const tickets = await makeZendeskRequest('/api/v2/group_memberships', 'GET')
+    const tickets = await makeZendeskRequest('/api/v2/tickets.json', 'GET')
     console.log(tickets)
 }
 

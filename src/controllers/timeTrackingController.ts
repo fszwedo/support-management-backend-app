b2157dcb77a008675ba5c8b316a1e9214c3fcbbe
FIlip <filip.szwedo@gmail.com>
2023-06-09 10:45:21 +0200

    removing obsolete newlines etc

--- src/controllers/timeTrackingController.ts
+++ src/controllers/timeTrackingController.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import getTicketAuditLogs from '../services/zendesk/getTicketAuditLogs';
 import getAgents from '../services/zendesk/getAgentsService';
-import zendeskRequest from "../services/zendesk/authenticationService";
+import zendeskRequestType from "../services/zendesk/authenticationService";
 import TimeTrackingEventService from '../services/timeTrackingService';
 import { TimeTrackingEvent } from '../models/timeTrackingEventModel';
 import { TICKET_CUSTOM_FIELDS } from '../CONSTANTS';
@@ -14,49 +14,50 @@ export default class TimeTrackingController {
         this.timeTrackingEventService = timeTrackingEventService
     }
 
-    saveNewTimeTrackingEvents = async (eventSearchStartDate?: Date) => {
+    saveNewTimeTrackingEvents = async (zendeskRequest: typeof zendeskRequestType, eventSearchStartDate?: Date) => {
         //get all users - to match time tracking submission with user
         const agents = await getAgents(zendeskRequest);
 
         //get newest tracking event from Mongo
         const lastTimeTrackingEvent = await this.timeTrackingEventService.getNewestTimeTrackingEvent();
 
-        //get the newest audit logs
+        //get the newest audit logs - with search date if it was passed to saveNewTimeTrackingEvents (so we can backfill older tickets in case of app failure)
         const auditLogs = await getTicketAuditLogs(eventSearchStartDate);
 
         //filter audit logs to get the ones from the newest tracking event from mongo (or all events, if theres no data in mongo)
-        const newAuditLogs = lastTimeTrackingEvent ? auditLogs.filter(log => new Date(log.created_at) > new Date(lastTimeTrackingEvent.created_at)) : auditLogs;
+        //we filter IF theres at least one event in db AND we dont have eventSearchStartDate (as otherwise we want to resave everything that's possible)
+        const newAuditLogs = lastTimeTrackingEvent && !eventSearchStartDate ? auditLogs.filter(log => new Date(log.created_at) > new Date(lastTimeTrackingEvent.created_at)) : auditLogs;
 
         //for each audit log - get events that are related to time tracking
         const timeTrackingEventsToSave: TimeTrackingEvent[] = [];
 
-        newAuditLogs.forEach(log => {
-            const timeSpentEvent = log.events.find(ev => ev.field_name === TICKET_CUSTOM_FIELDS.TIME_SPENT);
-            const totalTimeSpentEvent = log.events.find(ev => ev.field_name === TICKET_CUSTOM_FIELDS.TOTAL_TIME_SPENT)
+        if (newAuditLogs.length > 0) {
+            newAuditLogs.forEach(log => {
+                const timeSpentEvent = log.events.find(ev => ev.field_name == TICKET_CUSTOM_FIELDS.TIME_SPENT.toString());
+                const totalTimeSpentEvent = log.events.find(ev => ev.field_name == TICKET_CUSTOM_FIELDS.TOTAL_TIME_SPENT.toString())
 
-            if (timeSpentEvent && totalTimeSpentEvent) {
-                const timeTrackingEvent: TimeTrackingEvent = {
-                    created_at: new Date(log.created_at),
-                    zendeskEventId: timeSpentEvent.id,
-                    zendeskAuditLogId: log.id,
-                    userId: log.author_id,
-                    userName: agents.find(agent => agent.id === log.author_id).name,
-                    ticketId: log.ticket_id,
-                    timeSpent: parseInt(timeSpentEvent.value),
-                    totalTimeSpent: parseInt(totalTimeSpentEvent.value)
+                if (timeSpentEvent && totalTimeSpentEvent) {
+                    const timeTrackingEvent: TimeTrackingEvent = {
+                        created_at: new Date(log.created_at),
+                        zendeskEventId: timeSpentEvent.id,
+                        zendeskAuditLogId: log.id,
+                        userId: log.author_id,
+                        userName: agents.find(agent => agent.id === log.author_id).name,
+                        ticketId: log.ticket_id,
+                        timeSpent: parseInt(timeSpentEvent.value),
+                        totalTimeSpent: parseInt(totalTimeSpentEvent.value)
+                    }
+                    timeTrackingEventsToSave.push(timeTrackingEvent);
                 }
-
-                timeTrackingEventsToSave.push(timeTrackingEvent);
+            })
+            //save events in the db - if theres already an event with given eventID - skip!
+            try {
+                await this.timeTrackingEventService.saveTimeTrackingEvents(timeTrackingEventsToSave);
+            } catch (error) {
+                console.log(error)
+                //this doesnt work - leaving this for further investigation
+                //this is to catch e.g. errors when we're trying to save non-unique entries
             }
-        })
-        console.log(`${timeTrackingEventsToSave.length} events to be saved!`)
-        //save events in the db - if theres already an event with given eventID - skip!
-        try {
-            await this.timeTrackingEventService.saveTimeTrackingEvents(timeTrackingEventsToSave);
-        } catch (error) {
-            console.log(error)
-            //this doesnt work - leaving this for further investigation
-            //this is to catch e.g. errors when we're trying to save non-unique entries
         }
         return;
     }
@@ -92,7 +93,6 @@ export default class TimeTrackingController {
                 .send(csv);
             return;
         } catch (error) {
-            console.log(error)
             res.status(400).json({ message: 'There was an error during time tracking download!' });
         }
     }

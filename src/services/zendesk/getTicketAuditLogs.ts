a76fc93e48e87e6b97c3f05a44bd1f2b3711d02a
FIlip <filip.szwedo@gmail.com>
2023-06-09 13:23:01 +0200

    getTicketAudits service test setup

--- src/services/zendesk/getTicketAuditLogs.ts
+++ src/services/zendesk/getTicketAuditLogs.ts
@@ -3,14 +3,14 @@ import { auditLog } from "../../models/zendeskAuditLogModel";
 
 const getTicketAuditLogs = async (untilDate?: Date): Promise<auditLog[]> => {
     let ticketAuditsResponse = await makeZendeskRequest('/api/v2/ticket_audits', 'GET');
-    const audits = ticketAuditsResponse.audits;
+    const audits = ticketAuditsResponse.audits;    
 
-    if(untilDate){
-        while(untilDate < new Date(ticketAuditsResponse.audits.at(-1).created_at)){
+    if (untilDate) {
+        while (untilDate < new Date(ticketAuditsResponse.audits.at(-1).created_at)) {
             ticketAuditsResponse = await makeZendeskRequest(`/api/v2/ticket_audits.json?cursor=${ticketAuditsResponse.before_cursor}`, 'GET');
-            audits.push(...ticketAuditsResponse.audits);
+            audits.push(...ticketAuditsResponse.audits);            
         }
-    }    
+    }
     return audits;
 }
 
